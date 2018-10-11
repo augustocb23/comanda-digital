748dca071c979babf747ab9151834bb517583b91
Augusto César <augustocb@gmail.com>
2018-10-10 22:47:58 -0300

    Correção de codificação

--- src/main/java/main/security/SpringWebSecurityInitializer.java
+++ src/main/java/main/security/SpringWebSecurityInitializer.java
@@ -1,7 +1,20 @@
 package main.security;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+
+@Order(1)
 public class SpringWebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
+	@Override
+	protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+		FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter("encodingFilter",
+				new CharacterEncodingFilter());
+		characterEncodingFilter.setInitParameter("encoding", "UTF-8");
+		characterEncodingFilter.setInitParameter("forceEncoding", "true");
+		characterEncodingFilter.addMappingForUrlPatterns(null, false, "/*");
+	}
 }
